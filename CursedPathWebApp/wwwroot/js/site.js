6735d40c6ab9ff1f6e6f9c66542481253da4ead8
ISuckHardAtCoding <dwburkinator@gmail.com>
2017-10-08 21:12:21 -0400

    pleeeeeaaassseee

--- CursedPathWebApp/wwwroot/js/site.js
+++ CursedPathWebApp/wwwroot/js/site.js
@@ -1,1 +1,21 @@
 ﻿// Write your JavaScript code.
+$(function () {
+
+    var $sidebar = $("#sidebar"),
+        $window = $(window),
+        offset = $sidebar.offset(),
+        topPadding = 15;
+
+    $window.scroll(function () {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+
+});
